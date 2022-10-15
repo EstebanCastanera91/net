ec27d4280068b77378f624c502d3a620d052792d
Esteban Castañera <estebancastanera@gmail.com>
2022-10-15 03:51:26 -0300

    Ejercicio1

---
+++ DataAccess/UniversidadDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Universidad.Models.DataModels;
+
+namespace Universidad.DataAccess
+{
+    public class UniverdidadDBContext : DbContext
+    {
+        public UniverdidadDBContext(DbContextOptions<UniverdidadDBContext> options) : base(options)
+        {
+            //TODO:Add Dbsets(Tablas de nuestra BD)
+        }
+
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Curso>? Cursos { get; set; }
+    }
+}
---
+++ Models/DataModels/Curso.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Universidad.Models.DataModels
+{
+    public enum Nivel
+    {
+        Basico,
+        Intermedio,
+        Avanzado
+    }
+    public class Curso : BaseEntity
+    {
+
+
+        [Required, StringLength(50)]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required, StringLength(280)]
+        public string DescripcionCorta { get; set; } = string.Empty;
+
+        [Required]
+        public string DescripcionLarga { get; set; } = string.Empty;
+
+        [Required]
+        public string PublicoObjetivo { get; set; } = string.Empty;
+
+        [Required]
+        public string Objetivos { get; set; } = string.Empty; 
+
+        [Required]
+        public string Requisitos { get; set; } = string.Empty;
+
+        [Required]
+        public Nivel Niveles { get; set; } = Nivel.Basico;
+
+
+    }
+}
